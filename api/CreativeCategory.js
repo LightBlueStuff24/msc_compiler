b4e45028939d1e2a0a47071c855fe33b5ee85b70
Sifatplayz <90343181+Sifatplayz@users.noreply.github.com>
2023-12-02 14:41:37 +0600

    update api/CreativeCategory.js

--- api/CreativeCategory.js
+++ api/CreativeCategory.js
@@ -1,20 +1,20 @@
 
 class Category {
-    static Construction = "construction";
-    static Items = "items";
-    static Equipments = "Equipments";
-    static Nature = "nature";
-    static None = "none";
+  static Construction = "construction";
+  static Items = "items";
+  static Equipments = "Equipments";
+  static Nature = "nature";
+  static None = "none";
 }
 
 class Group {
-    static Planks = "itemGroup.planks.name";
-    static Slabs = "itemGroup.slabs.name";
-    static Stairs = "itemGroup.stairs.name";
-    static Fences = "itemGroup.fences.name";
-    static FenceGates = "itemGroup.fence_gates.name";
-    static Walls = "itemGroup.walls.name";
+  static Planks = "itemGroup.planks.name";
+  static Slabs = "itemGroup.slabs.name";
+  static Stairs = "itemGroup.stairs.name";
+  static Fences = "itemGroup.fences.name";
+  static FenceGates = "itemGroup.fence_gates.name";
+  static Walls = "itemGroup.walls.name";
 }
 
-exports.Category=Category;
-exports.Group=Group;
+exports.Category = Category;
+exports.Group = Group;
