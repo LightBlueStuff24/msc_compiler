8be4346dfd0961ddbbafe126b84c0babc9247937
Sifatplayz <90343181+Sifatplayz@users.noreply.github.com>
2023-12-02 16:35:44 +0600

    update api/Block.js

--- api/Block.js
+++ api/Block.js
@@ -9,7 +9,7 @@ class Block {
     "format_version": config["version"],
     "minecraft:block": {
       "description": {
-        "identifier": `${c["prefix"]}:${this.identifier.toLowerCase()}`,
+        "identifier": `${c["prefix"]}:${this.Identifier.toLowerCase()}`,
         "menu_category": {}
       },
       "components": {},
