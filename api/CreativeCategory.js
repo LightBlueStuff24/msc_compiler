330cc98d0d01216f1b4c2cb434a12976df6c91ca
Sifatplayz <90343181+Sifatplayz@users.noreply.github.com>
2023-12-04 11:37:19 +0600

    Formatted CreativeCategory.js

--- api/CreativeCategory.js
+++ api/CreativeCategory.js
@@ -1,5 +1,12 @@
 
 const validCategories = new Set(['construction','items','equipments','nature','none'])
-const validGroups = new Set(['itemGroup.planks.name','itemGroup.slabs.name','itemGroup.stairs.name','itemGroup.fences.name','itemGroup.fence_gates.name','itemGroup.walls.name'])
+const validGroups = new Set([
+  'itemGroup.planks.name',
+  'itemGroup.slabs.name',
+  'itemGroup.stairs.name',
+  'itemGroup.fences.name',
+  'itemGroup.fence_gates.name',
+  'itemGroup.walls.name']
+)
 exports.validCategories = validCategories
 exports.validGroups = validGroups
