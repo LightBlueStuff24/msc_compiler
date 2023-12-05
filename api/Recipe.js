faffe77b5609b70e02e6a19f904c6c2cec952ebb
Sifatplayz <90343181+Sifatplayz@users.noreply.github.com>
2023-12-05 13:31:17 +0600

    Added @handleUnlocks and @handleResults

--- api/Recipe.js
+++ api/Recipe.js
@@ -16,12 +16,13 @@ class ShapedRecipe {
   static Pattern;
   static Keys;
   static Unlocks;
-  static Result;
+  static Results;
+  static Priority = -1;
   /**
    * @CreatesShapedRecipeObject
    */
   static init() {
-    this.__Data["minecraft:recipe_shaped"]["description"]["identifier"]=`${this.name.toLowerCase()}`
+    this.__Data["minecraft:recipe_shaped"]["description"]["identifier"]=`${config["prefix"]}:${this.name.toLowerCase()}`
     /**
      * @handleTags
      */
@@ -36,17 +37,37 @@ class ShapedRecipe {
      * @handlePattern
      */
     if(this.Pattern) {
-      if(typeof this.Pattern != ("string"||"boolean"||"number"||"object"||"Function")) {
-        this.Pattern.forEach((p, i)=>{
-          if(typeof p == "string") {
-            this.__Data["minecraft:recipe_shaped"]["pattern"].push(p);
-          }
-          else return new Error(`[${this.name}] [propetry: Pattern] [index: ${i}]: expected type {string} instead found {${typeof p}}`)
-        })
-      }
-      else return new Error(`[${this.name}] [propetry: Pattern]: expected type {string[]} instead found {${typeof this.Pattern}}`)
+      if(typeof this.Pattern == ("string"||"boolean"||"number"||"object"||"Function")) return new Error(`[${this.name}] [propetry: Pattern]: expected type {string[]} instead found {${typeof this.Pattern}}`)
+      this.Pattern.forEach((p, i)=>{
+        if(typeof p == "string") return new Error(`[${this.name}] [propetry: Pattern] [index: ${i}]: expected type {string} instead found {${typeof p}}`)
+        this.__Data["minecraft:recipe_shaped"]["pattern"].push(p);
+      })
     }
-    
+    /**
+     * @handleUnlocks
+     */
+    if(this.Unlocks) {
+      if(typeof this.Unlocks == ("string"||"boolean"||"number"||"object"||"Function")) return new Error(`[${this.name}] [propetry: Unlocks]: expected type {string[]} instead found {${typeof this.Unlocks}}`)
+      this.__Data["minecraft:recipe_shaped"]["unlocks"]=[]
+      this.Unlocks.forEach((u, i)=>{
+        if(typeof u != "string") return new Error(`[${this.name}] [propetry: Unlocks] index: ${i}]: expected type {string} instead found {${typeof u}}`)
+        this.__Data["unlocks"].push({"item": u})
+      })
+    }
+    /**
+     * @handleResults
+     */
+    if(this.Results) {
+      if(typeof this.Results == ("string"||"boolean"||"number"||"object"||"Function")) return new Error(`[${this.name}] [propetry: Results]: expected type {string[]} instead found {${typeof this.Results}}`)
+      this.__Data["minecraft:recipe_shaped"]["results"]=[]
+      this.Results.forEach((u, i)=>{
+        if(typeof u != "object") return new Error(`[${this.name}] [propetry: Results] [[index: ${i}]: expected type {string} instead found {${typeof u}}`)
+        if(typeof u["Item"] != "string") return new Error(`[${this.name}] [propetry: Results] [index: ${i}] [child: Item]: expected type {string} instead found {${typeof u["Item"]}}`)
+        if(!u["Count"]) u["Count"] = 1;
+        if(typeof u["Count"] != "number") return new Error(`[${this.name}] [propetry: Results] [index: ${i}] [child: Count]: expected type {string} instead found {${typeof u["Count"]}}`)
+        this.__Data["minecraft:recipe_shaped"]["results"].push({"item": u["Item"], "count": u["Count"]})
+      })
+    }
     
     return JSON.stringify(this.__Data);
   }
