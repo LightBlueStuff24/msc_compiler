c7f5cab1e0fd09241fe81fedd210c5e6e11a8351
Redux <null>
2024-01-16 22:08:33 +0000

    Some Changes related to textureGen

--- src/textureGen/textureGen.js
+++ src/textureGen/textureGen.js
@@ -35,26 +35,17 @@ class TextureGenerator {
     }
     async generateFluidBucket(fluidTexture, fillRegion) {
         const bucketImage = await Jimp.read('../assets/textures/bucket_empty.png');
+        const maskImage = await Jimp.read('../assets/textures/bucket_mask.png')
         const fluidImage = await Jimp.read(fluidTexture);
+        fluidImage.mask(maskImage,0,0)
+        
+        
         const fillX = fillRegion[0];
         const fillY = fillRegion[1];
         const fillWidth = fillRegion[2] - fillRegion[0];
         const fillHeight = fillRegion[3] - fillRegion[1];
-        const fillArea = bucketImage.clone().crop(fillX, fillY, fillWidth, fillHeight);
-        const secondFillArea = bucketImage.clone().crop(3, 4, 10, 1);
-        fluidImage.scan(0, 0, fluidImage.getWidth(), fluidImage.getHeight(), function (x, y) {
-            const { r, g, b } = Jimp.intToRGBA(this.getPixelColor(x, y));
-            const [centerX, centerY] = [fillWidth / 2, fillHeight / 2]
-            const distance = Math.sqrt(((x - centerX) ** 2) + ((y - centerY) ** 2))
-            const brightness = (distance / Math.max(centerX, centerY)) * 255;
-            const alpha = Jimp.intToRGBA(fillArea.getPixelColor(x, y)).a;
-            const adjustedAlpha = brightness > 42 ? alpha : 255 - alpha;
-            fillArea.setPixelColor(Jimp.rgbaToInt(r, g, b, adjustedAlpha), x, y);
-            secondFillArea.setPixelColor(Jimp.rgbaToInt(r, g, b, adjustedAlpha), x, y);
-        });
-        bucketImage.blit(fillArea, fillX, fillY, 0, 0, fillWidth, fillHeight);
-        bucketImage.blit(secondFillArea, 3, 4, 0, 0, 10, 1);
-        return await bucketImage.getBufferAsync(Jimp.MIME_PNG);
+        bucketImage.blit(fluidImage, fillX, fillY);
+        bucketImage.write('./e.png')
     }
 
 
@@ -69,11 +60,7 @@ async function generateVariations() {
         const resultBuffer = await textureGenerator.genVariation(texturePath, baseColor);
         await writeFileAsync(`${process.cwd()}/variation_${i}.jpg`, resultBuffer);
     }
+    
 }
-
-(async () => {
-    const textureGenerator = new TextureGenerator();
-    const buffer = await textureGenerator.generateFluidBucket('./lava_still.png', [5, 3, 11, 6]);
-    await writeFileAsync(`${process.cwd()}/variation_bucket.png`, buffer);
-})();
-
+const textureGenerator = new TextureGenerator();
+textureGenerator.generateFluidBucket('./water_still.png',[5,3,11,6])
